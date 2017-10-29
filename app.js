219bdcd4bfc9d4ef56f070105ee0e813a55196d2
BonbonGit <maxibonnbon@gmail.com>
2017-10-29 21:06:00 +0100

    More efficient logs

--- app.js
+++ app.js
@@ -2,36 +2,34 @@ const http = require('http');
 const fs = require('fs');
 const io = require('socket.io')();
 
+console.log('Server started')
 const server = http.createServer((req, res) => {
-  console.log('Server started')
+  console.log('Incoming request for ' + req.url);
+
   if(req.url == '/' || req.url == '/index' || req.url == '/index.html'){
-    console.log('Incoming request for ' + req.url);
     fs.readFile(__dirname + '/html-css/index.html', 'utf-8', (error, content) => {
 
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(content);
 
-      console.log('Response given for ' + req.url);
     });
   } else if(req.url == '/html-css/roomList.html'){
-    console.log('Incoming request for ' + req.url);
     fs.readFile(__dirname + '/html-css/roomList.html', 'utf-8', (error, content) => {
 
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(content);
 
-      console.log('Response given for ' + req.url);
     });
   } else {
-    console.log('Incoming request for ' + req.url);
     fs.readFile(__dirname + '/html-css/template.html', 'utf-8', (error, content) => {
 
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(content);
 
-      console.log('Response given for ' + req.url);
     });
   }
+
+  console.log('Response given for ' + req.url);
 });
 
 io.listen(server);
