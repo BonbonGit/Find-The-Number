20f16dc824f586ca8ff354871b7eb0b7af6af3ff
BonbonGit <maxibonnbon@gmail.com>
2017-10-29 20:52:08 +0100

    Added clearer logs

--- app.js
+++ app.js
@@ -3,25 +3,33 @@ const fs = require('fs');
 const io = require('socket.io')();
 
 const server = http.createServer((req, res) => {
+  console.log('Server started')
   if(req.url == '/' || req.url == '/index' || req.url == '/index.html'){
+    console.log('Incoming request for ' + req.url);
     fs.readFile(__dirname + '/html-css/index.html', 'utf-8', (error, content) => {
-      console.log('C à index : ' + __dirname + 'html-css/index.html');
+
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(content);
 
+      console.log('Response given for ' + req.url);
     });
   } else if(req.url == '/html-css/roomList.html'){
+    console.log('Incoming request for ' + req.url);
     fs.readFile(__dirname + '/html-css/roomList.html', 'utf-8', (error, content) => {
-      console.log('C à roomList');
+
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(content);
 
+      console.log('Response given for ' + req.url);
     });
   } else {
+    console.log('Incoming request for ' + req.url);
     fs.readFile(__dirname + '/html-css/template.html', 'utf-8', (error, content) => {
-      console.log('C à template');
+
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(content);
+
+      console.log('Response given for ' + req.url);
     });
   }
 });
@@ -29,7 +37,7 @@ const server = http.createServer((req, res) => {
 io.listen(server);
 // Quand un client se connecte, on le note dans la console
 io.on('connection', (socket) => {
-  console.log('Un client est connecté !');
+  console.log('New player!');
 });
 
 server.listen(3000);
